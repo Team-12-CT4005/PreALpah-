7113445461a32477840dcd3a6c756ad764a7b894
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-05-26 22:19:18 +0100

    Added death script
    death script

---
+++ Gameplay_Loop_Test/Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersData.P1_Health <= 0 && playersData.P2_Health <= 0 && playersData.P3_Health <= 0 && playersData.P4_Health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
