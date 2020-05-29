bf14664ed88e878d8bbf02c986e4e3540cabb109
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-05-29 20:22:08 +0100

    s
    s

---
+++ Gameplay_Loop_Test/Assets/Scripts/Menu/Lobby.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lobby : MonoBehaviour
+{
+    public GameObject Object;
+    public static bool Paused2 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Confirm()
+    {
+        Object.SetActive(false);// deactivates the pause menu
+        Time.timeScale = 1f; // resumes normal time
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
